18f43b107d5b679963fb5af23ed005ce5596e9e3
Osher <solimani.osher@gmail.com>
2024-11-27 13:04:18 +0200

    Add some basic pages + router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { HomeLayout, Landing, Register, Login, Error } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
